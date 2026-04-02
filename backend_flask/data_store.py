05ae9a60cc4a78f513ee77a80a73440cafffa157
Selvin1998 <selvinchuc3@gmail.com>
2026-04-01 20:47:26 -0600

    Create data_store.py

---
+++ backend_flask/data_store.py
@@ -0,0 +1,14 @@
+# Almacenamiento temporal en memoria
+
+db = {
+    "admin": {
+        "username": "AdminPPCYL2",
+        "password": "AdminPPCYL2771",
+        "role": "admin"
+    },
+    "cursos": [],
+    "tutores": [],
+    "estudiantes": [],
+    "asignaciones_tutores": [],
+    "asignaciones_estudiantes": []
+}
