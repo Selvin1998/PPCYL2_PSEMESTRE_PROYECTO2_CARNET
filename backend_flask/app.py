0d3af391a7b9c03f096530ad8e9e9acf42abcf3e
Selvin1998 <selvinchuc3@gmail.com>
2026-04-01 20:12:20 -0600

    Update app.py

--- backend_flask/app.py
+++ backend_flask/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
+from routes.auth import auth_bp
 
 app = Flask(__name__)
 
+# Ruta de prueba
 @app.route('/health', methods=['GET'])
 def health():
     return jsonify({
@@ -9,5 +11,9 @@ def health():
         "message": "API funcionando correctamente"
     })
 
+# Registrar rutas (IMPORTANTE antes de correr)
+app.register_blueprint(auth_bp, url_prefix="/api")
+
+# Ejecutar servidor
 if __name__ == '__main__':
     app.run(debug=True)
